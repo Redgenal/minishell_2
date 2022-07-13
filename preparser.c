84eb7bca852cee2439f127afaf9ff2fb437d2d66
Glenda Antediluvian <gantedil@at-f2.msk.21-school.ru>
2022-07-13 20:34:03 +0300

    new add functions for preparsing

--- preparser.c
+++ preparser.c
@@ -6,61 +6,44 @@
 /*   By: gantedil <gantedil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 16:28:49 by gantedil          #+#    #+#             */
-/*   Updated: 2022/07/12 21:13:09 by gantedil         ###   ########.fr       */
+/*   Updated: 2022/07/13 20:33:23 by gantedil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// int	pre_quote(char *str)
-// {
-// 	int	i;
-// 	int	k;
-// 	int	j;
+int pre_end_slesh (char *str, int len)
+{
+	int j;
 
-// 	i = 1;
-// 	k = 0;
-// 	j = 0;
-// 	if (str[0] == '\'')
-// 		k ++;
-// 	if (str[0] == '\"')
-// 		j++;
-// 	while (str[i])
-// 	{
-// 		if (str[i] == '\'' && str[i - 1] != '\\')
-// 			k++;
-// 		if (str[i] == '\"' && str[i - 1] != '\\')
-// 			j++;
-// 		i++;
-// 	}
-// 	if (k % 2 != 0 && k > 0)
-// 	{
+	j = 0;
+	while (len >= 1 && str[len - 1])
+	{
+		len--;
+		j++;
+	}
+	if (j%2 == 0)
+		return (0);
+	return(1);
+}
 
-// 	}
-// 		return (1);
-// 	else
-// 		return (0);
-// }
+int pre_slesh (char *str, int i)
+{
+	int j;
 
-// int	pre_quote(char *str)
-// {
-// 	int	i;
-// 	int	o_flag;
-// 	int	d_flag;
+	j = 0;
+	i--;
+	while(str[i] == '\\' && i >= 0)
+	{	
+		j++;
+		i--;
+	}
+	if (j % 2 == 0)
+		return(0);
+	return (1);
+}
 
-// 	o_flag = -1;
-// 	d_flag = -1;
-// 	i = 1;
-// 	if (str[0] == '\'')
-// 		o_flag = -(o_flag);
-// 	if (str[0] == '\"')
-// 		d_flag = -(d_flag);
-// 	while (str[i])
-// 	{
-// 		if(
-// 	}
-	
-// }
+
 int	drop_null(char *str, int i, char c)
 {
 	if (str[i] && str[i] == c)
@@ -76,134 +59,187 @@ int	drop_null(char *str, int i, char c)
 	return (1);
 }
 
-int	find_double(char *str)
+int	find_double(char *str, int j)
 {
 	int	i;
 	int	drop;
 
-	i = 0;
 	drop = 0;
-
-
-	if (str[i] == '|' && str[i + 1] != '|' && str[i + 1] != '\0')
+	i = j;
+	if (str[i] == '>' && str[i + 1] == '>' && str[i + 1] != '\0' \
+		&& str[i + 2] == '>' && str[i + 2] != '\0')
 	{
+		if (str[i + 3] != '\0' && str[i + 3] == '>')
+		{
+			printf("Syntax error near unexpected token '>>'\n");
+			return (1);
+		}
+		printf("Syntax error near unexpected token '>'\n");
+		return (1);
+	}
+	if (str[i] == '<' && str[i + 1] == '<' && str[i + 1] != '\0' \
+		&& str[i + 2] == '<' && str[i + 2] != '\0')
+	{
+		if (str[i + 3] != '\0' && str[i + 3] == '<')
+		{
+			printf("Syntax error near unexpected token '<<'\n");
+			return (1);
+		}
+		printf("Syntax error near unexpected token '<'\n");
+		return (1);
+	}
+	if (str[i] == '<' && str[i + 1] != '\0' && str[i + 1] == '>')
+	{
+		printf("Syntax error near unexpected token '<>'\n");
+		return (1);
+	}
+	if (str[i] == '>' || str[i] == '<')
+	{
+		drop = drop_null(str, i + 1, '>');
+		if (drop == 1)
+		{
+			printf("Syntax error near unexpected token '>'\n");
+			return (1);
+		}
+		if (drop == 2)
+		{
+			printf("Syntax error near unexpected token '>>'\n");
+			return (1);
+		}
+		drop = drop_null(str, i + 1, '<');
+		if (drop == 1)
+		{
+			printf("Syntax error near unexpected token '<'\n");
+			return (1);
+		}
+		if (drop == 2)
+		{
+			printf("Syntax error near unexpected token '<<'\n");
+			return (1);
+		}
+	}
+	if (str[i] == '|' && str[i + 1] == '|' && str[i + 1] != '\0')
+	{
+		if (str[i + 2] != '\0' && str[i + 2] == '|')
+		{
+			printf("Syntax error near unexpected token '||'\n");
+			return (1);
+		}
+		printf("Missing || command processing\n");
+		return (1);
+	}
+	if (str[i] == '|')
+	{
+		if (drop_null(str, i + 1, '|'))
+		{
+			printf("Syntax error near unexpected token '|'\n");
+			return (1);
+		}
+	}
+	return (0);
+}
+
+int	first_part(char *str, int len, int i)
+{
+	if ((str[len - 1] == '>' || str[len - 1] == '<' \
+		|| (str[len - 1] == '>' && str[len - 2] == '>' && len >= 2) \
+		|| (str[len - 1] == '<' && str[len - 2] == '<' && len >= 2)))
+	{
+			printf("Syntax error near unexpected token 'newline'\n");
+			return (1);
+	}
+	if (str[len - 1] == '|' || (str[len - 1] == '|' && str[len - 2] != '|'))
+	{
+		if (len >= 3 && str[len - 3] != '|')
+		{	
+			printf("Multi-line comment processing missing\n");
+			return (1);
+		}
+		if (len >= 3 && str[len - 3] == '|')
+		{	
+			if (len >= 4 && str[len - 4] == '|')
+			{
+				printf("Syntax error near unexpected token '||'\n");
+				return (1);
+			}
+			printf("Syntax error near unexpected token '|'\n");
+			return (1);
+		}
+	}
+	
+	if (find_double(str, i))
+		return (1);
+
+	return (0);
+}
+
+int	pre_quote(char *str)
+{
+	int	i;
+	int	o_flag;
+	int	d_flag;
+	int len;
+	int res;
+
+	len = ft_strlen(str);
+	res = 0;
+	o_flag = -1;
+	d_flag = -1;
+	i = 0;
+	if (str[i] == '|')
+	{
+		if (str[i + 1] == '|' && len >= 1)
+		{
+			printf("Syntax error near unexpected token '||'\n");
+			return (1);
+		}
 		printf("Syntax error near unexpected token '|'\n");
 		return (1);
 	}
-
-
 	if (str[i] == '<' && str[i + 1] == '<' && str[i + 1] != '\0')
 	{
 		printf("Multi-line comment processing missing\n");
 		return (1);
 	}
-
-
 	while (str[i])
 	{
 
-
-		if (str[i] == '|' && str[i + 1] == '|' && str[i + 1] != '\0')
+		if (str[i] == '\'' && !pre_slesh(str, i))
 		{
-			if (str[i + 2] != '\0' && str[i + 2] == '|')
+			o_flag = -(o_flag);
+			i++;
+			while (str[i] && str[i] != '\'')
 			{
-				printf("Syntax error near unexpected token '||'\n");
-				return (1);
+				i++;
+				if (str[i] == '\'' && pre_slesh(str, i))
+					i++;
 			}
-			printf("Missing || command processing\n");
+			if (str[i] == '\'')
+				o_flag = -(o_flag);
+		}
+		if (str[i] == '\"' && !pre_slesh(str, i))
+		{
+			d_flag = -(d_flag);
+			i++;
+			while (str[i] && str[i] != '\"')
+			{
+				i++;
+				if (str[i] == '\"' && pre_slesh(str, i))
+					i++;
+			}
+			if (str[i] == '\"')
+				d_flag = -(d_flag);
+		}
+		res = first_part(str, len, i);
+		if (res == 1)
 			return (1);
-		}
-
-
-		if (str[i] == '>' && str[i + 1] == '>' && str[i + 1] != '\0' \
-			&& str[i + 2] == '>' && str[i + 2] != '\0')
-		{
-			if (str[i + 3] != '\0' && str[i + 3] == '>')
-			{
-				printf("Syntax error near unexpected token '>>'\n");
-				return (1);
-			}
-			printf("Syntax error near unexpected token '>'\n");
-			return (1);
-		}
-
-
-		if (str[i] == '<' && str[i + 1] == '<' && str[i + 1] != '\0' \
-			&& str[i + 2] == '<' && str[i + 2] != '\0')
-		{
-			if (str[i + 3] != '\0' && str[i + 3] == '<')
-			{
-				printf("Syntax error near unexpected token '<<'\n");
-				return (1);
-			}
-			printf("Syntax error near unexpected token '<'\n");
-			return (1);
-		}
-
-		if (str[i] == '<' && str[i + 1] != '\0' && str[i + 1] == '>')
-		{
-			printf("Syntax error near unexpected token '<>'\n");
-			return (1);
-		}
-
-		if (str[i] == '>' || str[i] == '<')
-		{
-			drop = drop_null(str, i + 1, '>');
-			if (drop == 1)
-			{
-				printf("Syntax error near unexpected token '>'\n");
-				return (1);
-			}
-			if (drop == 2)
-			{
-				printf("Syntax error near unexpected token '>>'\n");
-				return (1);
-			}
-			drop = drop_null(str, i + 1, '<');
-			if (drop == 1)
-			{
-				printf("Syntax error near unexpected token '<'\n");
-				return (1);
-			}
-			if (drop == 2)
-			{
-				printf("Syntax error near unexpected token '<<'\n");
-				return (1);
-			}
-		}
-
-		if (str[i] == '|')
-		{
-			if (drop_null(str, i + 1, '|'))
-			{
-				printf("Multi-line comment processing missing\n");
-				return (1);
-			}
-		}
-
-
 		i++;
 	}
-	return (0);
-}
-
-int	first_part(char *str, int len)
-{
-	if (pre_quote(str))
-		return (1);
-	if ((str[len - 1] == '>' || str[len - 1] == '<' \
-		|| (str[len - 1] == '>' && str[len - 2] == '>' && len >= 2) \
-		|| (str[len - 1] == '<' && str[len - 2] == '<' && len >= 2)))
+	if (d_flag > 0 || o_flag > 0)
 	{
-		if (!(len >= 3 && (str[len - 3] == '>' || str[len - 3] == '<')))
-		{
-			printf("Syntax error near unexpected token 'newline'\n");
-			return (1);
-		}
-	}
-	if (find_double(str))
+		printf ("Multi-line comment processing missing\n");
 		return (1);
-
+	}
 	return (0);
 }
 
@@ -215,6 +251,9 @@ int	ft_prepars(char *str)
 
 	i = 0;
 	len = ft_strlen(str);
-	res = first_part(str, len);
+	res = pre_quote(str);
 	return (res);
 }
+
+//Обработать сom >>>     com>>>>    com <<<<    com <<<<<
+// Добавить pre_end_slesh для обработки слеша в конце
